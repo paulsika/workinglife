859ca1bc1f28af6a29681131b9d534b9ebb89fea
paulsika <paul@paulsika.com>
2020-07-07 11:15:02 +0200

    more styling

--- pages.js
+++ pages.js
@@ -1,14 +1,10 @@
-
 /*
-
 
 Working Life, Living Work:
 Forms of Control in Johannesburg’s New Workplaces
-
-
-
 */
 
+var startTime = performance.now()
 
 function title_old () {
 
@@ -85,6 +81,7 @@ function un_highlight_current_menu_button() {
 function highlight_current_menu_button(page) {
     var id = ""
     switch( page.id ) {
+        case "intro_page": id = "intro_button"; break; 
         case "moles_page": id = "moles_button"; break; 
         case "neoliberal_page": id = "neoliberal_button";break; 
         case "blurred_page": id = "blurred_button" ; break;     
@@ -147,6 +144,46 @@ function template_page(name, title_src, text) {
     return page ; 
 }
 
+function template_page2(name, title_src, html_src) {
+    let page = document.createElement("div"); 
+  //  page.id = "blurred_page";   
+    page.id = name + "_page";
+    /*
+    let title = document.createElement("p");
+    title.id = "blurred_page_title";   
+    title.innerHTML = "Blurred Lines"; 
+    title.className = "essay_title";
+    */
+   let title = document.createElement("img");   
+    title.id = page.id + "_title"; 
+    title.src = title_src ; 
+ title.className = "essay_title";
+
+ page.appendChild(title); 
+
+    let essay = document.createElement("div"); 
+   //essay.id = "blurred_essay" ; 
+     essay.id = name + "_essay"
+    essay.className= "essay" ; 
+   // essay.innerHTML = blurred_text ; 
+
+
+    let object = document.createElement("object")
+    object.type = "text/html"
+    object.data =  html_src
+    object.className = "essay_text"
+    console.log("OBJECT: ", object)
+
+    //essay.innerHTML = object ; 
+    essay.appendChild(object)
+    console.log("ESSAY DIV: ", essay)
+
+//    essay.readOnly = true; 
+
+    page.appendChild(essay) ; 
+    return page ; 
+}
+
 
 
 function moles_page() { 
@@ -170,35 +207,12 @@ function neoliberal_page() {
 }
 
 function blurred_page() {
-    let page = document.createElement("div"); 
-    page.id = "blurred_page";   
-    /*
-    let title = document.createElement("p");
-    title.id = "blurred_page_title";   
-    title.innerHTML = "Blurred Lines"; 
-    title.className = "essay_title";
-    */
-
-   let title = document.createElement("img");
-   title.id = "blurred_page_title";   
-   title.src = "images/blurred_lines.png"; 
-   title.className = "essay_title";
     
-    
-    page.appendChild(title); 
-
-    let essay = document.createElement("textarea"); 
-    essay.id = "blurred_essay" ; 
-    essay.className= "essay" ; 
-    essay.innerHTML = blurred_text ; 
-
-    //essay.cols = 60 ;
-    //essay.rows = 20 ; 
-    essay.readOnly = true; 
-
-    page.appendChild(essay) ; 
-    return page ; 
+    let blurred_page = template_page2("blurred", "images/blurred_lines.png", "essay.html")
+    return blurred_page
 }
+
+
 
 function working_page() { 
     let page = document.createElement("div"); 
@@ -236,9 +250,8 @@ function menu() {
 
     let menu = document.createElement("div");    menu.id = "menu"; 
 
-
     let intro_page = template_page("intro", "images/introduction_radial.png", blurred_text)
-    let blurred_page = template_page("blurred", "images/blurred_lines.png", blurred_text)
+    let blurred_page = template_page2("blurred", "images/blurred_lines.png", "essay.html")
     let moles_page = template_page("moles", "images/moles_to_serpents.png", blurred_text)
     let neoliberal_page = template_page("neoliberal", "images/neoliberal_reason_and_realism.png", blurred_text)
     let working_page = template_page("working", "images/working_life_living_work.png", blurred_text)
@@ -260,10 +273,15 @@ function menu() {
 function clock() {
     let clock = document.createElement("p"); 
     clock.id = "clock"; 
+
+
+    console.log("START TIME: ", startTime)
+
+
     return clock  ;
 }
 
-function update_clock(p) {
+function update_clock_old(p) {
     let real_hour = p.hour() ; let real_minute = p.minute() ; let real_second = p.second() ;
     
     let displayed_hour = real_hour ;
@@ -276,6 +294,26 @@ function update_clock(p) {
     clock.innerHTML = displayed_time ;
 }
 
+function update_clock(p) {
+    
+    let elapsed_time = Math.floor( (performance.now() - startTime) / 1000 ) 
+   // console.log("ELAPSED TIME", elapsed_time)
+    
+    let elapsed_second = elapsed_time % 60
+    //console.log("ELAPSED SECONDS: ", elapsed_second)
+    let elapsed_minute = Math.floor(elapsed_time / 60 )
+    let elapsed_hour = Math.floor(elapsed_time / 3600)
+
+    let displayed_time = elapsed_hour.toString().padStart(2,'0') + " : " 
+                        +  elapsed_minute.toString().padStart(2,'0') + " : "
+                         + elapsed_second.toString().padStart(2,'0') ;
+    
+    let clock = document.getElementById("clock") ; 
+    clock.innerHTML = displayed_time ;
+}
+
+
+
 
 function camera() {
     let camera = document.createElement("img"); 
@@ -284,7 +322,7 @@ function camera() {
 }
 
 //NOTE: work on smoother movement 
-function update_camera_position(p) {
+function update_camera_position_old(p) {
 
     let camera = document.getElementById("camera");
    // let canvasHeight = document.getElementById("canvas").height ; 
@@ -295,4 +333,18 @@ function update_camera_position(p) {
     if (cameraY <= lower_bound) {   cameraY = lower_bound ;  }
 
     camera.style.top = cameraY + "%"
+} 
+
+function update_camera_position(p) {
+
+    let camera = document.getElementById("camera");
+   // let canvasHeight = document.getElementById("canvas").height ; 
+    let upper_bound = 70.0 ;  let lower_bound = 5.0  ;
+    var cameraY = ( p.mouseY * 90.0) / canvasHeight  ;//magic number
+
+    if (cameraY >= upper_bound)  {  cameraY = upper_bound ; }
+    if (cameraY <= lower_bound) {   cameraY = lower_bound ;  }
+
+    //camera.style.top = cameraY + "%"
+
 } 
